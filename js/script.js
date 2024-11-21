9f81dcc6f9fe3e90671f2047805769a7ae964464
elfffffy <wjd1278@naver.com>
2024-11-21 23:03:02 +0900

    href 변경

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let doorBtn = document.querySelector('#page0 .start');
 window.localStorage.clear();
 
 doorBtn.addEventListener('click', () => {
-    window.location.href = "../intro.html";
+    window.location.href = "/holiday_juicemaker/intro.html";
 })
